eb6a73e39b723e398ecc23d4693f106c3a48ef6d
Will <you@example.com>
2019-10-19 13:30:59 -0300

    primeiro commit

---
+++ vuejs/desafio-01/app.js
@@ -0,0 +1,8 @@
+new Vue({
+    el: '#desafio',
+    data:{
+        nome: 'William',
+        idade: '33',
+        imagem: 'https://i2.wp.com/viladosilicio.com.br/wp-content/uploads/2018/01/vuejs-logo.jpg'
+    }
+})
